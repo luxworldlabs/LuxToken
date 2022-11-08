a0dbd1ba4d21dc54816cf3a135ec5541371f645b
luxworldlabs <app@lux.world>
2022-11-08 14:19:32 +0700

    updated token name

--- LuxToken.sol
+++ LuxToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.15;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract LuxWorldToken is ERC20("Lux World", "Lux") {
+contract LuxWorldToken is ERC20("LuxWorld", "Lux") {
   constructor() {
     _mint(msg.sender, 2_000_000_000 ether);
   }
