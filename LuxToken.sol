ac148a6a37da1dd8020f2ab7bbb705d14151e0c4
luxworldlabs <app@lux.world>
2022-11-08 17:01:18 +0700

    updated requirement

--- LuxToken.sol
+++ LuxToken.sol
@@ -1,10 +1,28 @@
-//SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LuxWorldToken is ERC20("LuxWorld", "Lux") {
-  constructor() {
-    _mint(msg.sender, 2_000_000_000 ether);
-  }
+contract LuxWorldToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("LuxWorld", "LUX") {
+        _mint(msg.sender, 2_000_000_000 ether);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual override(ERC20) {
+        super._transfer(sender, recipient, amount);
+    }
 }
